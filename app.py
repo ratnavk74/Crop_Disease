d0bdbcb372771349cbb8234e3bf9b765dd0e6680
Ratna <ratnavk74@gmail.com>
2026-05-11 12:38:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,22 @@
-from flask import Flask, render_template, request
-import tensorflow as tf
-import numpy as np
-import cv2
+from flask import Flask
+from flask import render_template
+from flask import request
 import os
-from disease_info import DISEASE_INFO
+
+from utils.prediction import predict_disease
+from utils.recommendation import get_recommendation
 
 app = Flask(__name__)
 
-# Load trained model
-model = tf.keras.models.load_model('crop_disease_model.h5')
-
-# Classes
-classes = [
-    'Maize_Blight',
-    'Maize_Common_Rust',
-    'Maize_Healthy',
-    'Wheat_Healthy',
-    'Wheat_Rust',
-    'Wheat_Septoria'
-]
-
 UPLOAD_FOLDER = 'static/uploads'
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# Preprocess image
-
-def preprocess_image(path):
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (224, 224))
-    img = img / 255.0
-    img = np.expand_dims(img, axis=0)
-    return img
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -43,24 +24,39 @@ def predict():
     file = request.files['image']
 
     if file:
-        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+
+        filepath = os.path.join(
+            UPLOAD_FOLDER,
+            file.filename
+        )
+
         file.save(filepath)
 
-        processed_image = preprocess_image(filepath)
+        disease, confidence = predict_disease(filepath)
 
-        prediction = model.predict(processed_image)
-        predicted_class = classes[np.argmax(prediction)]
-        confidence = round(np.max(prediction) * 100, 2)
+        info = get_recommendation(disease)
 
-        info = DISEASE_INFO[predicted_class]
+        if confidence >= 90:
+            severity = 'High'
+
+        elif confidence >= 70:
+            severity = 'Medium'
+
+        else:
+            severity = 'Low'
 
         return render_template(
             'result.html',
-            disease=predicted_class,
+            disease=disease,
             confidence=confidence,
+            severity=severity,
             cause=info['cause'],
             symptoms=info['symptoms'],
             treatment=info['treatment'],
             prevention=info['prevention'],
             image_path=filepath
+        )
+
+
+if __name__ == '__main__':
     app.run(debug=True)
